045e16beeb01c4fd2f02b55582b1a6e354f03186
khanhcdt <khanhc7308@gmail.com>
2022-07-23 16:29:55 +0700

    update file linker_List.c

--- BT3_Linkerlist/Linker_List.c
+++ BT3_Linkerlist/Linker_List.c
@@ -5,17 +5,16 @@ struct Node
 {
     int data;
     struct Node *pNext;
-    
 };
-typedef struct  Node *node;
+typedef struct Node *node;
 node create_Node(int data)
 {
     node temp;
     temp = (node)malloc(sizeof(struct Node));
-    temp ->data = data;
-    temp ->pNext = NULL;
+    temp->data = data;
+    temp->pNext = NULL;
 }
-node Push_pack(node addressNode,int data)
+node Push_pack(node addressNode, int data)
 {
     node temp, p;
     temp = create_Node(data);
@@ -30,40 +29,62 @@ node Push_pack(node addressNode,int data)
         {
             p = p->pNext;
         }
-      p->pNext =temp;
+        p->pNext = temp;
     }
     return addressNode;
 }
-void Change_Data(node DL, int index,int data)
+void Change_Data(node DL, int index, int data)
 {
-    int i= 0;
+    int i = 0;
     node change = NULL;
     change = DL;
     while (change != NULL && i != index)
     {
-        change = change ->pNext;
+        change = change->pNext;
         i++;
     }
-    change ->data = data;
+    change->data = data;
+}
+void delete_Node(node dt)
+{
+    node del,res;
+    del = dt;
+    while (del ->pNext!= NULL)
+    {
+        del = del->pNext;
+    }
+    while (dt ->pNext != del)
+    {
+        dt = dt->pNext; 
+    }
+    dt->pNext = NULL;
+    free(del);
+}
+void OutPut_DL(node Output)
+{
+    node p;
+    p = Output;
+    for (int  i = 0; p != NULL; p= p->pNext)
+    {
+        printf("%d\n",p->data);
+    }
     
 }
-
 int main()
-{   node DL = NULL;
+{
+    node DL = NULL;
     int n, data;
     printf("nhap n: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        printf("nhap data [%d] ",i);
-        scanf("%d",&data);
-        DL = Push_pack(DL,data);
+        printf("nhap data [%d] ", i);
+        scanf("%d", &data);
+        DL = Push_pack(DL, data);
     }
-    Change_Data(DL,2,10);
-    for (int i = 0; DL != NULL; DL = DL ->pNext)
-    {
-        printf("%d\n",DL->data);
-    }
-    
-    return 0 ;
+    Change_Data(DL, 2, 10);
+    OutPut_DL(DL);
+    delete_Node(DL);
+    OutPut_DL(DL);
+    return 0;
 }
